cb16bde84af67314e57685be3efd1c5b95e57679
calderonroberto <roberto@robertocalderon.ca>
2015-03-05 23:12:11 -0800

    fixing aws

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -3,7 +3,11 @@ class Thing < ActiveRecord::Base
   has_one :loan
 
   #NOTE: when deployed in heroku it defaults to development.
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :storage => :s3,:s3_credentials => S3_CREDENTIALS
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :storage => :s3,:s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
